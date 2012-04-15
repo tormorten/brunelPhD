b6448fc3a86bb528f0272855a1e42cb4fc899ee5
Tor-Morten Grønli <tormorteng@gmail.com>
2012-04-15 23:00:56 +0200

    Extra

---
+++ experiment_3/sensor-library/src/ac/uk/brunel/sensor/dto/SensorConstants.java
@@ -0,0 +1,14 @@
+package ac.uk.brunel.sensor.dto;
+
+
+public enum SensorConstants {
+    ;
+
+    public static final String INTENT_KEY_SENSOR = "sensorname";
+
+    static final String TYPE_TEXT_ACCELEROMETER = "Accelerometer";
+    static final String TYPE_TEXT_MAGNETIC_FIELD = "Magnetic field";
+    static final String TYPE_TEXT_ORIENTATION = "Orientation";
+    static final String TYPE_TEXT_PROXIMITY = "Proximity";
+    static final String TYPE_TEXT_LIGHT = "Light";
+}
